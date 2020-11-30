343a22393e62d7dde1f7df2d581216db4ef35792
Cody Maheu <cody.maheu@navy.mil>
2020-11-29 17:16:12 -0800

    For Roomee <3

--- src/components/RecipeDetail.js
+++ src/components/RecipeDetail.js
@@ -2,16 +2,6 @@ import React from 'react';
 import { View, Image, Text, StyleSheet } from 'react-native';
 
 const RecipeDetail = ({ result }) => {
-  // const { name, image_url, rating, review_count } = result;
-  // return (
-  //   <View style={styles.container}>
-  //     <Image style={styles.image} source={{ uri: image_url }} />
-  //     <Text style={styles.name}>{name}</Text>
-  //     <Text>
-  //       {rating} Stars, {review_count} Reviews{' '}
-  //     </Text>
-  //   </View>
-  // );
   const { title, image } = result;
   return (
     <View style={styles.container}>
