81a7ceff4594255d77c5d4ec36348f0de9d58b06
Cody Maheu <cody.maheu@navy.mil>
2020-12-11 17:53:52 -0800

    fixed styling errors on emailsent

--- src/screens/EmailSentScreen.js
+++ src/screens/EmailSentScreen.js
@@ -1,5 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, TouchableOpacity, Dimensions } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  Dimensions,
+  StatusBar,
+} from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const dimensions = Dimensions.get('window');
 const { width } = dimensions;
@@ -7,22 +16,31 @@ const { height } = dimensions;
 
 const EmailSent = ({ navigation }) => {
   return (
-    <View style={styles.container}>
-      <Image source={require('../../images/email_sent_logo.png')} />
-      <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-        <Text style={{ fontfamily: 'Robot' }}> Go back to </Text>
+    <SafeAreaView style={styles.somecontainer}>
+      <StatusBar barstyle="light-content" />
+      <View style={styles.container}>
+        <Image source={require('../../images/email_sent_logo.png')} />
+        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+          <Text style={{ fontfamily: 'robot' }}> Go back to </Text>
 
-        <TouchableOpacity onPress={() => navigation.navigate('Login')}>
-          <Text style={{ fontfamily: 'roboto', fontWeight: 'bold' }}>login</Text>
-        </TouchableOpacity>
+          <TouchableOpacity onPress={() => navigation.navigate('Login')}>
+            <Text style={{ fontfamily: 'roboto', fontWeight: 'bold' }}>login</Text>
+          </TouchableOpacity>
 
-        <Text style={{ fontfamily: 'roboto', paddingLeft: width * 0.01 }}>after verification.</Text>
+          <Text style={{ fontfamily: 'roboto', paddingLeft: width * 0.01 }}>
+            after verification.
+          </Text>
+        </View>
       </View>
-    </View>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
+  somecontainer: {
+    flex: 1,
+    backgroundColor: 'black',
+  },
   container: {
     paddingTop: height * 0.15,
     flex: 1,
